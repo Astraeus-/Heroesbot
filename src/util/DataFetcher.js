bca4850563337d89986ca0a237f7f9d539359039
Astraeus- <Astraeus-@users.noreply.github.com>
2018-05-09 14:30:25 +0200

    Updated DataFetcher
    - Added error logging when it fails to fetch the data

--- src/util/DataFetcher.js
+++ src/util/DataFetcher.js
@@ -1,15 +1,22 @@
 const FileHandler = require('./FileHandler.js')
 const heroesloungeApi = require('heroeslounge-api')
 const path = require('path')
+const Logger = require('./Logger.js')
 
 module.exports.allTeamData = () => {
   heroesloungeApi.getTeams().then((teams) => {
+    Logger.info('Updating Teamdata cache')
     FileHandler.writeJSONFile(path.join(__dirname, '../Data/Teamdata.json'), teams)
+  }).catch((error) => {
+    Logger.warn(`Could not update Teamdata cache\n${error}`)
   })
 }
 
 module.exports.matchesToday = () => {
   heroesloungeApi.getMatchesToday().then((matches) => {
+    Logger.info('Updating MatchesToday cache')
     FileHandler.writeJSONFile(path.join(__dirname, '../Data/MatchesToday.json'), matches)
+  }).catch((error) => {
+    Logger.warn(`Could not update MatchesToday cache\n${error}`)
   })
 }
