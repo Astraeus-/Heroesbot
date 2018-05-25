cdef7a09c0cf507f25e6189326b5d1d3b5001304
Astraeus- <Astraeus-@users.noreply.github.com>
2018-05-25 13:15:20 +0200

    Update config
    Now allows bot to use everyone and here mentions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const config = require('./config.json')
 const cron = require('node-cron')
 const DataFetcher = require('./util/DataFetcher.js')
 
-const bot = new Client(config.token)
+const bot = new Client(config.token, {
+  disableEveryone: false
+})
 bot.launch()
 
 cron.schedule('0 0 * * *', () => {
