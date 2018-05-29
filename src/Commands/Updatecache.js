ff821a079ac16ea524c5b9d1e09ac7ae4fbe1f5d
Astraeus- <Astraeus-@users.noreply.github.com>
2018-05-29 16:26:28 +0200

    Added updatecache command
    - Updates the local cache files

---
+++ src/Commands/Updatecache.js
@@ -0,0 +1,41 @@
+const BaseCommand = require('../Classes/BaseCommand.js')
+const DataFetcher = require('../util/DataFetcher.js')
+
+class Updatecache extends BaseCommand {
+  constructor (bot) {
+    const permissions = {
+      'Test-Server': {
+        channels: ['robotchannel'],
+        roles: ['Admin'],
+        users: []
+      }
+    }
+
+    const options = {
+      prefix: '!',
+      command: 'updatecache',
+      description: 'Updates the Heroesbot cache files.',
+      ignoreInHelp: true
+    }
+
+    super(permissions, options)
+    this.bot = bot
+  }
+
+  exec (msg) {
+    let TeamData = DataFetcher.allTeamData()
+    let MatchesToday = DataFetcher.matchesToday()
+
+    Promise.all([TeamData, MatchesToday]).then(() => {
+      this.bot.getDMChannel(msg.author.id).then((channel) => {
+        channel.createMessage('Updated cache files!')
+      })
+    }).catch((error) => {
+      this.bot.getDMChannel(msg.author.id).then((channel) => {
+        channel.createMessage(`Could not update cache files\n\`\`\`js\n${error}\n\`\`\``)
+      })
+    })
+  }
+}
+
+module.exports = Updatecache
