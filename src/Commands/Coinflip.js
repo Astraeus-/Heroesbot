fc366cf8f25a3bd5e473824e42553b33824b51b6
Astraeus- <Astraeus-@users.noreply.github.com>
2018-08-09 18:02:18 +0200

    Enable coinflip in nexus brawls server

--- src/Commands/Coinflip.js
+++ src/Commands/Coinflip.js
@@ -15,6 +15,11 @@ class Coinflip extends BaseCommand {
         channels: ['match_lounge_1', 'match_lounge_2', 'match_lounge_3', 'match_lounge_4', 'match_lounge_5', 'match_lounge_6', 'match_lounge_7', 'match_lounge_8', 'match_lounge_9', 'match_lounge_10', 'match_lounge_11', 'match_lounge_12', 'match_lounge_13', 'match_lounge_14', 'match_lounge_15', 'match_lounge_16', 'match_lounge_17', 'match_lounge_18', 'match_lounge_19', 'match_lounge_20', 'devops', 'mockdrafts'],
         roles: [],
         users: []
+      },
+      'HotS - Nexus Brawls': {
+        channels: ['match_lounge_1', 'match_lounge_2'],
+        roles: [],
+        users: []
       }
     }
 
