478c795424c6ff4c367f7d2533d8e06c2ef12ccb
Astraeus- <Astraeus-@users.noreply.github.com>
2018-08-13 20:17:32 +0200

    Minor fixes to Draft command
    Allow use in Heroes Lounge server
    Added additional info on how to determine 1st pick

--- src/Commands/Draft.js
+++ src/Commands/Draft.js
@@ -10,6 +10,11 @@ class Draft extends BaseCommand {
         channels: ['robotchannel'],
         roles: ['Admin'],
         users: []
+      },
+      'Heroes Lounge': {
+        channels: ['mockdrafts'],
+        roles: [],
+        users: []
       }
     }
 
@@ -18,7 +23,7 @@ class Draft extends BaseCommand {
       command: 'draft',
       aliases: ['mock', 'mockdraft'],
       description: 'Creates a Heroes of the Storm mockdraft.',
-      syntax: 'draft [map] [teamA] [teamB]\nThe map is specialised by all first letters e.g.: Battlefield of Eternity = BOE',
+      syntax: 'draft [map] [team 1] [team 2] [r]\nThe map is specialised by all first letters e.g.: Battlefield of Eternity = BOE.\nteam 1 has first pick by default.',
       min_args: 3
     }
 
