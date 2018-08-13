f641c0dda5d9819263bfd93c86d493c67f3f38d3
Astraeus- <Astraeus-@users.noreply.github.com>
2018-08-13 20:01:13 +0200

    Added draft command
    Allows you to create mockdrafts for a specified map for two teams. Returns links for both teams, as well as observer

---
+++ src/Commands/Draft.js
@@ -0,0 +1,169 @@
+const BaseCommand = require('../Classes/BaseCommand')
+const Logger = require('../util/Logger.js')
+
+const http = require('http')
+
+class Draft extends BaseCommand {
+  constructor (bot) {
+    const permissions = {
+      'Test-Server': {
+        channels: ['robotchannel'],
+        roles: ['Admin'],
+        users: []
+      }
+    }
+
+    const options = {
+      prefix: '!',
+      command: 'draft',
+      aliases: ['mock', 'mockdraft'],
+      description: 'Creates a Heroes of the Storm mockdraft.',
+      syntax: 'draft [map] [teamA] [teamB]\nThe map is specialised by all first letters e.g.: Battlefield of Eternity = BOE',
+      min_args: 3
+    }
+
+    super(permissions, options)
+    this.bot = bot
+  }
+
+  exec (msg, args) {
+    if (args.length < this.min_args) {
+      return this.bot.getDMChannel(msg.author.id)
+        .then((channel) => channel.createMessage('Invalid number of arguments'))
+        .catch((error) => {
+          Logger.warn('Could not inform invalid number of arguments', error)
+        })
+    }
+
+    const embed = {
+      color: this.bot.embed.color,
+      footer: this.bot.embed.footer,
+      title: '',
+      fields: [
+        {
+          name: 'Team One',
+          value: '',
+          inline: true
+        },
+        {
+          name: 'Team Two',
+          value: '',
+          inline: true
+        },
+        {
+          name: 'Observer',
+          value: ''
+        }
+      ]
+    }
+
+    const map = args[0].toUpperCase()
+    const teamA = args[1]
+    const teamB = args[2]
+
+    // Check that the map input actually exists.
+    if (!checkMap(map)) {
+      return this.bot.getDMChannel(msg.author.id)
+        .then((channel) => channel.createMessage(`Channel with abbreviation: ${map} does not exist`))
+        .catch((error) => {
+          Logger.warn('Could not inform about invalid map syntax', error)
+        })
+    }
+
+    createDraft(map, teamA, teamB).then((draft) => {
+      const battleground = draft.map.toLowerCase().replace(/\s/g, '-')
+
+      embed.title += `${draft.map}`
+      embed.fields[0].value = `[${draft.teams[0].name}](${draft.teams[0].url})`
+      embed.fields[1].value = `[${draft.teams[1].name}](${draft.teams[1].url})`
+      embed.fields[2].value = `[Observer](${draft.viewer.url})`
+      embed.thumbnail = {
+        'url': `https://heroeslounge.gg/themes/HeroesLounge-Theme/assets/img/maps/bg_${battleground}.jpg`
+      }
+
+      return embed
+    }).then((embed) => {
+      return msg.channel.createMessage({embed: embed}).catch((error) => {
+        throw error
+      })
+    }).catch((error) => {
+      Logger.error('Unable to create mockdraft', error)
+    })
+  }
+}
+
+let checkMap = (map) => {
+  // Heroes of the Storm map list.
+  /* eslint-disable */
+  const maps = [
+    'AP',     // Alterac Pass
+    'BOE',    // Battlefield of Eternity
+    'BB',     // Blackheart's Bay
+    'BH',     // Braxis Holdout
+    'CH',     // Cursed Hollow
+    'DS',     // Dragon Shire
+    'GOT',    // Garden of Terror
+    'H',      // Hanamura
+    'HM',     // Haunted Mines
+    'IS',     // Infernal Shrines
+    'ST',     // Sky Temple
+    'TOTSQ',  // Tomb of the Spider Queen
+    'TOD',    // Towers of Doom
+    'VF',     // Volskaya Foundry
+    'WJ'      // Warhead Junction
+  ]
+  /* eslint-enable */
+
+  return maps.includes(map)
+}
+
+let createDraft = (map, teamA, teamB) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'nexus-brawls.com',
+      port: 1338,
+      path: '/draft',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const requestData = {
+      'team1': teamA,
+      'team2': teamB,
+      'map': map
+    }
+
+    const req = http.request(options, (res) => {
+      let rawResponse = ''
+      res.setEncoding('utf8')
+
+      res.on('data', (d) => {
+        rawResponse += d
+      })
+
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          try {
+            let response = JSON.parse(rawResponse)
+            resolve(response)
+          } catch (err) {
+            reject(Error('Parse JSON response'))
+          }
+        } else {
+          reject(Error(`status Code ${res.statusCode} : Invalid request`))
+        }
+      })
+    })
+
+    req.on('error', (error) => {
+      reject(error)
+    })
+
+    req.write(JSON.stringify(requestData))
+    req.end()
+  })
+}
+
+module.exports = Draft
