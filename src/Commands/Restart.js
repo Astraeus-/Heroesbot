20ed2160b37f4d325b93f11e051eb54642f2d483
Astraeus- <Astraeus-@users.noreply.github.com>
2018-08-30 13:14:49 +0200

    Updated restart command
    Now takes an argument for what you'd like to restart
    
    network -> restart network services
    all -> restarts entire device

--- src/Commands/Restart.js
+++ src/Commands/Restart.js
@@ -1,6 +1,8 @@
 const BaseCommand = require('../Classes/BaseCommand.js')
 const Logger = require('../util/Logger.js')
 
+const { exec } = require('child_process')
+
 class Restart extends BaseCommand {
   constructor (bot) {
     const permissions = {
@@ -14,20 +16,49 @@ class Restart extends BaseCommand {
     const options = {
       prefix: '!',
       command: 'restart',
-      description: 'Restarts the host OS.',
-      ignoreInHelp: true
+      description: 'Restarts elements of thee host OS.',
+      syntax: 'restart <option>\nOptions are: network, all',
+      ignoreInHelp: true,
+      min_args: 1
     }
 
     super(permissions, options)
     this.bot = bot
   }
 
-  exec (msg) {
+  exec (msg, args) {
     if (this.bot.user.id === '321643992624267265') return
-    Logger.info('Restarting Heroesbot and host, please wait...')
-    require('child_process').exec('sudo /sbin/reboot', (msg) => {
-      Logger.info(msg)
-    })
+    if (args.length < this.min_args) {
+      return this.bot.getDMChannel(msg.author.id)
+        .then((channel) => channel.createMessage('Invalid number of arguments'))
+        .catch((error) => {
+          Logger.warn('Could not inform invalid number of arguments', error)
+        })
+    }
+
+    if (args[0] === 'all') {
+      Logger.info('Restarting Heroesbot host, please wait...')
+      exec('sudo /sbin/reboot', (error) => {
+        if (error) Logger.error('Error restarting Heroesbot', error)
+      })
+    } else if (args[0] === 'network') {
+      Logger.info('Restarting Heroesbot networking, please wait...')
+      exec('sudo ifconfig wlan0 down', (error) => {
+        if (error) Logger.error('Error disabling networking service Heroesbot', error)
+      })
+
+      setTimeout(() => {
+        exec('sudo ifconfig wlan0 up', (error) => {
+          if (error) Logger.error('Error enabling networking service Heroesbot', error)
+        })
+      }, 3000)
+    } else {
+      this.bot.getDMChannel(msg.author.id)
+        .then((channel) => channel.createMessage(`Incorrect command **${this.prefix + this.command}** syntax \nCommand usage: ${this.syntax}`))
+        .catch((error) => {
+          Logger.warn('Could not inform invalid syntax', error)
+        })
+    }
   }
 }
 
