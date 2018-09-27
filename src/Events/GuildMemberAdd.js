e03975ec68ed69f1779666315c67ab4375188aae
Astraeus- <Astraeus-@users.noreply.github.com>
2018-09-27 10:09:23 +0200

    Disable region sync on member join
    Too expensive of an operation for now

--- src/Events/GuildMemberAdd.js
+++ src/Events/GuildMemberAdd.js
@@ -29,22 +29,22 @@ module.exports = (bot) => {
 
     // Assign the EU or NA role for returning Discord members.
     // Currently uses the expensive operation of searching through all of our sloths.
-    heroesloungeApi.getSloths().then((sloths) => {
-      let returningSloth = sloths.find((sloth) => {
-        return sloth.discord_id === member.user.id
-      })
-
-      if (returningSloth) {
-        let regionRoleId
-        if (returningSloth.region_id === '1') {
-          regionRoleId = '494534903547822105' // EU
-        } else if (returningSloth.region_id === '2') {
-          regionRoleId = '494535033722372106' // NA
-        }
-
-        bot.addGuildMemberRole(guild.id, member.user.id, regionRoleId)
-          .catch(error => Logger.error(`Unable to reassign region role to ${member.user.username}`, error))
-      }
-    })
+    // heroesloungeApi.getSloths().then((sloths) => {
+    //   let returningSloth = sloths.find((sloth) => {
+    //     return sloth.discord_id === member.user.id
+    //   })
+    //
+    //   if (returningSloth) {
+    //     let regionRoleId
+    //     if (returningSloth.region_id === '1') {
+    //       regionRoleId = '494534903547822105' // EU
+    //     } else if (returningSloth.region_id === '2') {
+    //       regionRoleId = '494535033722372106' // NA
+    //     }
+    //
+    //     bot.addGuildMemberRole(guild.id, member.user.id, regionRoleId)
+    //       .catch(error => Logger.error(`Unable to reassign region role to ${member.user.username}`, error))
+    //   }
+    // })
   })
 }
