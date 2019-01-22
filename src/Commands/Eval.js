1a7a99baaab02c61eed052fc5f2cc8a30f7fb2d2
Astraeus- <Astraeus-@users.noreply.github.com>
2019-01-22 21:34:16 +0100

    Updated eval command to inspect result and allow eval\'ing async code

--- src/Commands/Eval.js
+++ src/Commands/Eval.js
@@ -1,5 +1,6 @@
 const BaseCommand = require('../Classes/BaseCommand.js')
 const Logger = require('../util/Logger.js')
+const util = require('util')
 
 class Eval extends BaseCommand {
   constructor (bot) {
@@ -22,16 +23,25 @@ class Eval extends BaseCommand {
     this.bot = bot
   }
 
-  exec (msg, args) {
+  async exec (msg, args) {
     const input = args.join(' ')
     let evalOutput
     try {
       /* eslint-disable */
-      evalOutput = eval(input)
+      evalOutput = await eval(input)
       /* eslint-enable */
-      if (evalOutput.toString().length > 2000) throw Error(`Eval output is too large: ${evalOutput.toString().length}`)
 
-      msg.channel.createMessage(`\`\`\`js\n${evalOutput}\`\`\``)
+      let inspectionDepth = 3
+      let inspectedOutput
+
+      do {
+        inspectionDepth--
+        inspectedOutput = util.inspect(evalOutput, { depth: inspectionDepth })
+      } while (inspectedOutput.length > 2000 && inspectionDepth > -1)
+
+      if (inspectedOutput.length > 2000) throw Error(`Eval output is too large: ${inspectedOutput.length}`)
+
+      msg.channel.createMessage(`\`\`\`js\n${inspectedOutput}\`\`\``)
         .catch(error => Logger.error(`Could not send eval result`, error))
     } catch (error) {
       msg.channel.createMessage(`Could not evaluate input properly\n${error}`)
