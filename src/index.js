9bac1582e2dffd0bc5ea5f349ded581380b7c8ff
Astraeus- <Astraeus-@users.noreply.github.com>
2019-03-01 13:44:06 +0100

    Changed "bot" to "client" for logical naming

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ const { token } = require('./config.json')
 const cron = require('node-cron')
 const { syncRegionRoles } = require('./util/SyncRoles.js')
 
-const bot = new Client(token, {
+const client = new Client(token, {
   getAllUsers: true,
   disableEveryone: false
 })
-bot.launch()
+client.launch()
 
 cron.schedule('0 */2 * * *', () => {
-  syncRegionRoles(bot.bot)
+  syncRegionRoles(client.bot)
 })
