e2722ba926f7e8eb160bf85e47204c2f02e468e9
Astraeus- <Astraeus-@users.noreply.github.com>
2019-03-13 20:44:50 +0100

    Remove legacy IRL role assignment code

--- src/Events/MessageReactionRemove.js
+++ src/Events/MessageReactionRemove.js
@@ -1,14 +1,6 @@
-const Logger = require('../util/Logger.js')
+// const Logger = require('../util/Logger.js')
 
 module.exports = (bot) => {
   bot.on('messageReactionRemove', (msg, emoji, userID) => {
-    if (msg.id === '449186816466092032' && emoji.name === 'PartyParrot') {
-      const guild = msg.channel.guild.id
-      if (bot.guilds.get(guild).roles.get('449093576065024001')) {
-        bot.removeGuildMemberRole(guild, userID, '449093576065024001')
-      } else {
-        Logger.warn(`IRL Talk role does not exist`)
-      }
-    }
   })
 }
