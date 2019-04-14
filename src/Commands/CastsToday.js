a68652af816da337dda827037a03a6eb98fa5f2a
Astraeus- <Astraeus-@users.noreply.github.com>
2019-04-14 23:03:13 +0200

    Update casts command to allow multiple regions

--- src/Commands/CastsToday.js
+++ src/Commands/CastsToday.js
@@ -23,9 +23,9 @@ class CastsToday extends BaseCommand {
       prefix: '!',
       command: 'caststoday',
       aliases: ['casts'],
-      description: 'Lists all of today\'s upcoming casts.',
-      syntax: 'caststoday',
-      cooldown: 30000,
+      description: 'Lists all of today\'s upcoming casts for the given region.',
+      syntax: 'caststoday <Region>',
+      cooldown: 15000,
       ignoreInHelp: true
     }
 
@@ -33,58 +33,81 @@ class CastsToday extends BaseCommand {
     this.bot = bot
   }
 
-  exec (msg) {
-    heroesloungeApi.getMatchesToday().then(async (matches) => {
+  exec (msg, args) {
+    const region = args[0].toLowerCase()
+
+    const regions = {
+      'eu': 'Europe/Berlin',
+      'na': 'America/Los_Angeles'
+    }
+
+    if (!regions[region]) {
+      return this.bot.getDMChannel(msg.author.id).then((channel) => {
+        return channel.createMessage(`The region ${args[0]} is not permitted`)
+      })
+    }
+
+    heroesloungeApi.getMatchesToday(regions[region]).then(async (matches) => {
       if (matches.length === 0) return null
 
-      let castedMatches = matches.filter((match) => {
-        return match.channel_id
-      })
-
-      if (castedMatches.length === 0) return null
-
-      castedMatches.sort((a, b) => {
-        return parseInt(a.wbp.slice(-8, -3).replace(':', '')) - parseInt(b.wbp.slice(-8, -3).replace(':', ''))
+      matches.sort((a, b) => {
+        return a.wbp > b.wbp
       })
 
       let matchDivisions = []
       let matchTeams = []
+      let matchChannels = []
       let matchCasters = []
 
-      for (let match in castedMatches) {
-        matchDivisions[match] = castedMatches[match].div_id ? heroesloungeApi.getDivision(castedMatches[match].div_id).catch((error) => {
+      for (let match in matches) {
+        matchDivisions[match] = matches[match].div_id ? heroesloungeApi.getDivision(matches[match].div_id).catch((error) => {
           Logger.warn('Unable to get division info', error)
         }) : ''
 
-        matchTeams[match] = heroesloungeApi.getMatchTeams(castedMatches[match].id).catch((error) => {
+        matchTeams[match] = heroesloungeApi.getMatchTeams(matches[match].id).catch((error) => {
           Logger.warn('Unable to get match team info', error)
         })
 
-        matchCasters[match] = heroesloungeApi.getMatchCasters(castedMatches[match].id).catch((error) => {
+        matchCasters[match] = heroesloungeApi.getMatchCasters(matches[match].id).catch((error) => {
           Logger.warn('Unable to get caster info', error)
+        })
+
+        matchChannels[match] = heroesloungeApi.getMatchChannels(matches[match].id).catch((error) => {
+          Logger.warn('Unable to get match channel info', error)
         })
       }
 
-      let response = ''
+      let response = ``
 
-      for (let match in castedMatches) {
+      for (let match in matches) {
         const division = await matchDivisions[match]
-        const casters = await matchCasters[match]
         const teams = await matchTeams[match]
+        const casters = await matchCasters[match]
+        const channels = await matchChannels[match]
 
-        let casterMessage = ''
+        // Skip uncasted matches.
+        if (channels.length === 0 || casters.length === 0) continue
 
-        // Attach all of the casters to the casterMessage.
+        let casterList = ''
+        let channelList = ''
+
+        // Attach all of the casters to the casterList.
         casters.forEach((caster) => {
-          if (casterMessage.length > 0) casterMessage += ' and '
-          casterMessage += `${caster.title}`
+          if (casterList.length > 0) casterList += ' and '
+          casterList += `${caster.title}`
+        })
+
+        // Attach all of the channels to the channelList.
+        channels.forEach((channel) => {
+          if (channelList.length > 0) channelList += ' and '
+          channelList += `**${channel.title}**: <${channel.url}>`
         })
 
         // Attach a division name or tournament + group.
         let fixture = ''
 
-        if (division.playoff_id || castedMatches[match].playoff_id) {
-          const playoff = castedMatches[match].playoff_id ? await heroesloungeApi.getPlayoff(castedMatches[match].playoff_id).catch((error) => {
+        if (division.playoff_id || matches[match].playoff_id) {
+          const playoff = matches[match].playoff_id ? await heroesloungeApi.getPlayoff(matches[match].playoff_id).catch((error) => {
             Logger.warn('Unable to get playoff info', error)
           }) : division.playoff_id ? await heroesloungeApi.getPlayoff(division.playoff_id).catch((error) => {
             Logger.warn('Unable to get playoff info', error)
@@ -95,25 +118,20 @@ class CastsToday extends BaseCommand {
           fixture = division.title
         }
 
-        const time = dateformat(new Date(castedMatches[match].wbp), 'HH:mm')
-        let twitchChannel
-
-        if (castedMatches[match].channel_id) {
-          twitchChannel = await heroesloungeApi.getTwitchChannel(castedMatches[match].channel_id).catch((error) => {
-            Logger.warn('Unable to get twitch channel info', error)
-          })
-        }
+        const dateElements = matches[match].wbp.match(/\d+/g)
+        let localMatchTime = new Date(Date.UTC(dateElements[0], dateElements[1], dateElements[2], dateElements[3], dateElements[4], dateElements[5]))
+        let time = region === 'na' ? dateformat(new Date(localMatchTime.toLocaleString('Ger', { timeZone: regions[region] })), 'hh:mm:A') : dateformat(new Date(localMatchTime.toLocaleString('Ger', { timeZone: regions[region] })), 'HH:mm:')
 
         // Group all match statement with the same time together.
-        if (match > 0 && castedMatches[match].wbp.slice(-8, -3) > castedMatches[match - 1].wbp.slice(-8, -3)) {
+        if (match > 0 && matches[match].wbp > matches[match - 1].wbp) {
           response += `\nAt ${time}\n`
-        } else if (match > 0 && castedMatches[match].wbp.slice(-8, -3) === castedMatches[match - 1].wbp.slice(-8, -3)) {
+        } else if (match > 0 && matches[match].wbp === matches[match - 1].wbp) {
           response += ''
         } else {
           response += `At ${time}\n`
         }
 
-        response += `${casterMessage} will be bringing you a ${fixture} match between ${teams[0].title} and ${teams[1].title} on \`${twitchChannel.url}\`\n`
+        response += `${casterList} will be bringing you a ${fixture} match between ${teams[0].title} and ${teams[1].title} on ${channelList}\n`
       }
 
       return response
@@ -122,7 +140,7 @@ class CastsToday extends BaseCommand {
         if (!response) {
           return channel.createMessage('There are no casted matches')
         } else {
-          return channel.createMessage(response)
+          return channel.createMessage(`Region time of: ${regions[region]}\n\n ${response}`)
         }
       })
     }).catch((error) => {
