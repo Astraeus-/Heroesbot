32543b25d81f000c24b1a887b0a8e4a22d7096f0
Astraeus- <Astraeus-@users.noreply.github.com>
2019-04-17 19:22:07 +0200

    Minor fix region sync

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const Client = require('./Client.js')
 const { token } = require('./config.json')
 const cron = require('node-cron')
-const { syncRegionRoles } = require('./util/SyncRoles.js')
 
 const client = new Client(token, {
   getAllUsers: true,
