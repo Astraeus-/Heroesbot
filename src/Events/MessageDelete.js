e10a7b89b5b34d15801d01b54fc4acae82fcc9b3
Astraeus- <Astraeus-@users.noreply.github.com>
2019-04-20 15:15:06 +0200

    Updated logging message deletion

--- src/Events/MessageDelete.js
+++ src/Events/MessageDelete.js
@@ -5,16 +5,17 @@ const { Logger } = require('../util.js')
 
 module.exports = (bot) => {
   bot.on('messageDelete', (msg) => {
-    let deletedMessage = msg.cleanContent ? msg.cleanContent : 'Uncached message'
-
     if (msg.channel.guild && msg.channel.guild.id === defaultServer) {
       msg.channel.guild.getAuditLogs(1, null, 72).then((auditlogs) => {
-        const invoker = auditlogs.entries[0].user.username
-        webhook.send({
-          title: 'Message Deleted',
-          color: 16711680,
-          description: `${invoker} deleted:\n${deletedMessage}`
-        })
+        const invoker = msg.channel.guild.members.get(auditlogs.entries[0].user.id)
+
+        if (msg.cleanContent && invoker.permission.has('manageMessages') && msg.author && msg.author.id !== invoker.id) {
+          webhook.send({
+            title: 'Message Deleted',
+            color: 16711680,
+            description: `${invoker.username} deleted:\n${msg.cleanContent}`
+          })
+        }
       }).catch((error) => {
         Logger.warn(`Unable to retrieve audit logs for guild: ${msg.channel.guild.name}`, error)
       })
