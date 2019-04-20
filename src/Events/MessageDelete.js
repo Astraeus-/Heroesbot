91f91e96bf219336628ccc76b610dc7306943650
Astraeus- <Astraeus-@users.noreply.github.com>
2019-04-20 22:05:34 +0200

    Hopefully fix delete logging

--- src/Events/MessageDelete.js
+++ src/Events/MessageDelete.js
@@ -7,13 +7,14 @@ module.exports = (bot) => {
   bot.on('messageDelete', (msg) => {
     if (msg.channel.guild && msg.channel.guild.id === defaultServer) {
       msg.channel.guild.getAuditLogs(1, null, 72).then((auditlogs) => {
-        const invoker = msg.channel.guild.members.get(auditlogs.entries[0].user.id)
+        const entry = auditlogs.entries[0]
 
-        if (msg.cleanContent && invoker.permission.has('manageMessages') && msg.author && msg.author.id !== invoker.id) {
+        // Checks that the last entry is deleting from the same user that deleted this message
+        if (msg.author && msg.author.id === entry.targetID) {
           const webhookResponse = {
             title: 'Message Delete',
             color: 16711680,
-            description: `Deleted by: ${invoker.username}\nChannel: ${msg.channel.name}`
+            description: `Deleted by: ${entry.user.username}\nChannel: ${entry.channel.name}`
           }
 
           let embeds = [
@@ -37,6 +38,7 @@ module.exports = (bot) => {
               })
             }
           }
+
           webhook.send(webhookResponse, embeds)
         }
       }).catch((error) => {
