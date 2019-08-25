2724d63137a07e7f5a1876420e94debf329112d9
Astraeus- <Astraeus-@users.noreply.github.com>
2019-08-25 19:44:58 +0200

    Remove standard add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'warn',
+            { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }
+        ]
+    }
+};
