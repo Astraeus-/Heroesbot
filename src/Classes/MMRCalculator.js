95f56fa867c8e8dfd98399ce33fc6d398d98a84d
Astraeus- <Astraeus-@users.noreply.github.com>
2020-07-06 16:51:19 +0200

    Statically define gameMode data for MMR calculator

--- src/Classes/MMRCalculator.js
+++ src/Classes/MMRCalculator.js
@@ -1,51 +1,35 @@
 
 class MMRCalculator {
-
   static calculateHeroesProfileAverageMMR(ratings) {
     if (ratings.size === 0) return 2800;
 
     let totalMMR = 0;
     let divider = 0;
 
-    const weightsMMR = {
-      'Storm League': {
-        weight: 0.7,
-        min_games: 10
-      },
-      'Unranked Draft': {
-        weight: 0.3,
-        min_games: 20
-      },
-      'Quick Match': {
-        weight: 1,
-        min_games: 25
-      }
-    };
+    const modes = Object.keys(this.gameModes);
 
-    const weights = Object.keys(weightsMMR);
-
-    for (let weight of weights) {
+    for (let mode of modes) {
       if ((ratings.size === 2 && ratings.has('Quick Match') && divider > 0) || divider === 1) break;
 
-      const modeRating = ratings.get(weight);
-      const weightData = weightsMMR[weight];
+      const modeRating = ratings.get(mode);
+      const modeData = this.gameModes[mode];
 
-      if (modeRating && modeRating.games_played > weightData.min_games) {
-        totalMMR += modeRating.mmr * weightData.weight;
-        divider += weightData.weight;
+      if (modeRating && modeRating.games_played > modeData.min_games) {
+        totalMMR += modeRating.mmr * modeData.weight;
+        divider += modeData.weight;
       }
     }
 
     if (totalMMR === 0 && divider === 0) {
-      for (let weight of weights) {
+      for (let mode of modes) {
         if ((ratings.size === 2 && ratings.has('Quick Match') && divider > 0) || divider === 1) break;
         
-        const modeRating = ratings.get(weight);
-        const weightData = weightsMMR[weight];
+        const modeRating = ratings.get(mode);
+        const modeData = this.gameModes[mode];
         
         if (modeRating) {
-          totalMMR += modeRating.mmr * weightData.weight;
-          divider += weightData.weight;
+          totalMMR += modeRating.mmr * modeData.weight;
+          divider += modeData.weight;
         }
       }
     }
@@ -54,19 +38,19 @@ class MMRCalculator {
   }
 
   static getRatingsHeroesProfile(data) {
-    const gameModes = ['Quick Match', 'Unranked Draft', 'Storm League'];
+    const modes = ['Quick Match', 'Unranked Draft', 'Storm League'];
     const ratings = new Map();
     
-    for (let gameMode of gameModes) {
+    for (let gameMode of modes) {
       const modeData = data[gameMode];
     
-      if (gameMode === 'Storm League' && modeData && modeData.games_played >= 10) {
+      if (gameMode === 'Storm League' && modeData && modeData.games_played >= this.gameModes[gameMode].min_games) {
         modeData.active = true;
         ratings.set(gameMode, modeData);
-      } else if (gameMode === 'Unranked Draft' && modeData && modeData.games_played >= 20) {
+      } else if (gameMode === 'Unranked Draft' && modeData && modeData.games_played >= this.gameModes[gameMode].min_games) {
         modeData.active = true;
         ratings.set(gameMode, modeData);
-      } else if (gameMode === 'Quick Match' && modeData && modeData.games_played >= 25) {
+      } else if (gameMode === 'Quick Match' && modeData && modeData.games_played >= this.gameModes[gameMode].min_games) {
         modeData.active = true;
         ratings.set(gameMode, modeData);
       }
@@ -74,7 +58,7 @@ class MMRCalculator {
     
     // None of the game modes met the minimum games played requirement.
     if (ratings.size === 0) {
-      for (let gameMode of gameModes) {
+      for (let gameMode of modes) {
         const modeData = data[gameMode];
         if (modeData) {
           ratings.set(gameMode, modeData);
@@ -85,5 +69,20 @@ class MMRCalculator {
     return ratings;
   }
 }
+
+MMRCalculator.gameModes = {
+  'Storm League': {
+    weight: 0.7,
+    min_games: 10
+  },
+  'Unranked Draft': {
+    weight: 0.3,
+    min_games: 20
+  },
+  'Quick Match': {
+    weight: 1,
+    min_games: 25
+  }
+};
 
 module.exports = MMRCalculator;
