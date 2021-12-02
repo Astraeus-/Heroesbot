299b0289ba41c8cef65b28540898067ab18282b9
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-02 21:50:18 +0100

    Types: Add some placeholder types

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,25 @@ export interface Match {
     playoff_winner_next?: number;
     playoff_loser_next?: number;
 }
+
+export interface Region {
+    name: string;
+    timezone: string | null;
+    heroesloungeId: 1 | 2 | null;
+    blizzardRegion: '1' | '2' | '3' | '5';
+  }
+
+export interface Sloth {
+    id: number;
+    title: string;
+    discord_id: string;
+    discord_tag: string;
+    region_id: number;
+}
+
+export interface TwitchChannel {
+    id: number;
+    title: string;
+    url: string;
+    pivot: {match_id: number, channel_id: number};
+}
