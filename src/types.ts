18f17d01508865dfe9c997530fdb161e2a6208a7
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-03 15:02:03 +0100

    Meta: Remove unused duplicate types

--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,6 @@
-export interface Match {
-    id: number;
-    div_id?: number;
-    playoff_id?: number
-    winner_id: number;
-    round?: number;
-    is_played: boolean;
-    schedule_date: string,
-    tbp: string,
-    wbp: string;
-    playoff_postion?: number;
-    playoff_winner_next?: number;
-    playoff_loser_next?: number;
-}
-
 export interface Region {
     name: string;
     timezone: string | null;
     heroesloungeId: 1 | 2 | null;
     blizzardRegion: '1' | '2' | '3' | '5';
   }
-
-export interface Sloth {
-    id: number;
-    title: string;
-    discord_id: string;
-    discord_tag: string;
-    region_id: number;
-}
-
-export interface TwitchChannel {
-    id: number;
-    title: string;
-    url: string;
-    pivot: {match_id: number, channel_id: number};
-}
